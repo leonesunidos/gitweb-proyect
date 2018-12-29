6090ffa98d2e75919a3fa9cb45f9274174c3792c
aleluis777 <luislinaresburnes@gmail.com>
2018-12-29 02:07:54 -0500

    commit final con nodejs

--- controllers/rutasController.js
+++ controllers/rutasController.js
@@ -2,12 +2,84 @@
 const controller = {};
 controller.indexGet = function(req, res){
     console.log('index');
-    res.sendFile(__dirname+ '/views/index.html');
+    res.render('pages/index');
+}
+
+controller.aboutGet = function(req, res){
+    console.log('index');
+    res.render('pages/about');
+}
+
+controller.blog_detail_leftbarGet = function(req, res){
+    console.log('blog-detail-leftbar');
+    res.render('pages/blog-detail-leftbar');
+}
+
+controller.blog_detail_rightbarGet = function(req, res){
+    console.log('blog_detail_rightbar');
+    res.render('pages/blog-detail-rightbar');
+}
+
+controller.blog_detailGet = function(req, res){
+    console.log('blog_detail');
+    res.render('pages/blog-detail');
+}
+
+controller.blog_with_leftbarGet = function(req, res){
+    console.log('blog_with_leftbar');
+    res.render('pages/blog-with-leftbar');
+}
+
+controller.blog_with_rightbarGet = function(req, res){
+    console.log('blog_with_rightbar');
+    res.render('pages/blog-with-rightbar');
+}
+
+controller.blogGet = function(req, res){
+    console.log('blog');
+    res.render('pages/blog');
 }
 
 controller.contactGet = function(req, res){
-    console.log('index');
-    res.sendFile(__dirname + '/views/contactGet.html');
+    console.log('contact');
+    res.render('pages/contact');
 }
+
+controller.portfolio_detailGet = function(req, res){
+    console.log('portfolio-detail');
+    res.render('pages/portfolio-detail');
+}
+
+controller.portfolioGet = function(req, res){
+    console.log('portfolio');
+    res.render('pages/portfolio');
+}
+
+controller.priceGet = function(req, res){
+    console.log('price');
+    res.render('pages/price');
+}
+
+controller.services_detailGet = function(req, res){
+    console.log('services-detail');
+    res.render('pages/services-detail');
+}
+
+controller.aboutGet = function(req, res){
+    console.log('about');
+    res.render('pages/about');
+}
+
+controller.servicesGet = function(req, res){
+    console.log('services');
+    res.render('pages/services');
+}
+
+controller.teamGet = function(req, res){
+    console.log('team');
+    res.render('pages/team');
+}
+
+
 
 module.exports = controller;
--- routes/rutas.js
+++ routes/rutas.js
@@ -4,8 +4,23 @@ const rutasController = require('../controllers/rutasController');
 
 
 //Rutas de alarmas
+router.get('/index', rutasController.indexGet);
 router.get('/', rutasController.indexGet);
-router.get('/prueba', rutasController.contactGet);
+router.get('/blog-detail-leftbar', rutasController.blog_detail_leftbarGet);
+router.get('/blog-detail-rightbar', rutasController.blog_detail_rightbarGet);
+router.get('/blog-detail', rutasController.blog_detailGet);
+router.get('/blog-with-leftbar', rutasController.blog_with_leftbarGet);
+router.get('/blog-with-rightbar', rutasController.blog_with_rightbarGet);
+router.get('/blog', rutasController.blogGet);
+router.get('/contact', rutasController.contactGet);
+router.get('/portfolio-detail', rutasController.portfolio_detailGet);
+router.get('/portfolio', rutasController.portfolioGet);
+router.get('/price', rutasController.priceGet);
+router.get('/services-detail', rutasController.services_detailGet);
+router.get('/about', rutasController.aboutGet);
+router.get('/services', rutasController.servicesGet);
+router.get('/team', rutasController.teamGet);
+
 
 
 
